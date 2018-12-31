0dd13cb24109047e7df06935e6d6b8c0ac99f98f
Milton Reis <milton.reis@kroton.com.br>
2018-12-31 17:13:23 -0200

    Ajuste para rodar com consul

--- centralized-swagger-docs/documentation-app/src/main/java/com/satish/central/docs/DocumentationAppApplication.java
+++ centralized-swagger-docs/documentation-app/src/main/java/com/satish/central/docs/DocumentationAppApplication.java
@@ -2,6 +2,7 @@ package com.satish.central.docs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableScheduling
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DocumentationAppApplication {
 
 	public static void main(String[] args) {
